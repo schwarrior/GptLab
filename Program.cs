97ae2a0e65ace5f3d63bf446028687442a8cfd6f
Robert Schwartz <robschwartz@prettyapps.net>
2026-03-04 12:51:34 -0500

    working api call candidate

--- Program.cs
+++ Program.cs
@@ -6,28 +6,30 @@ using Microsoft.SemanticKernel;
 
 Console.WriteLine("GPT Lab Starting...");
 
+var apiKeyId = "OpenAI:ApiKey";
 var config = new ConfigurationBuilder()
     .AddUserSecrets<Program>()
     .Build();
-
-
-var apiKeyId = "OpenAI:ApiKey";
 var apiKey = config[apiKeyId];
-
 Debug.Assert(!string.IsNullOrEmpty(apiKey), $"API key not found. Please add it to user secrets with the key '{apiKeyId}'.");
-
 Console.WriteLine($"Loaded {apiKeyId}");
 
-// var builder = Kernel.CreateBuilder();
+string? question = null;
+while (string.IsNullOrEmpty(question))
+{
+	Console.WriteLine();
+	Console.WriteLine("Please enter your question for the model:");
+	question = Console.ReadLine();
+	Console.WriteLine();
+}
 
-// builder.AddOpenAIChatCompletion(
-//     modelId: "gpt-4o-mini", // inexpensive + good for dev
-//     apiKey: apiKey
-// );
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion(
+    modelId: "gpt-4o-mini", // inexpensive + good for dev
+    apiKey: apiKey
+);
+var kernel = builder.Build();
+var result = await kernel.InvokePromptAsync(question);
 
-// var kernel = builder.Build();
-
-// var result = await kernel.InvokePromptAsync("Explain what RAG is in 3 sentences.");
-
-// Console.WriteLine(result);
+Console.WriteLine(result);
 
